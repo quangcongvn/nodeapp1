bd613be4e567ba489adc108a27fc7cf9e913dab6
quangcongvn <congtk1992@gmail.com>
2019-02-24 11:30:54 +0000

    add helmet security

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ const port = 4001
 app.get('/', (req, res) => res.send('test git clone'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+/// Cong add more
+
+// assets folder (like PHP assets)
+app.use(express.static('public'))
+// security
+var helmet = require('helmet')
+app.use(helmet())
